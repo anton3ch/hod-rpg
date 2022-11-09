ffcaf5f6426f87ae2f0fec607a266dd699ae4df6
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-11-09 17:54:23 -0600

    add test for branching statements of monster and character hits
    Co-authored-by: Aitana Shough <aeshough@gmail.com>

--- __test__/battle.test.js
+++ __test__/battle.test.js
@@ -10,6 +10,7 @@ describe('Battlefield', () => {
   beforeEach(() => { 
     charSkills = new Skills();
     larysCharacter = new Character("Larys", "Strong", charSkills);
+    larysCharacter.addHouseBonus();
     iceDragon = new Monster("Ice Dragon", "ice", 8);
     battle = new Battlefield(larysCharacter, iceDragon);
   });
@@ -25,15 +26,61 @@ Health:
 20HP vs 10HP`);
   });
 
-  // battle(larysCharacter, iceDragon) {
-  //   this.hero = 
-  //   this.monster =
-  // }
+  test('should allow character to hit monster according to strength stat', () => {
+    expect(battle.characterHit()).toEqual("Ice Dragon Hit! Remaining Health: 4");
+  });
 
-  // test('should have the character "hit" the monster and reduce its health', () => {
-  //   initiateBattle(larysCharacter, iceDragon);
-  //   expect(iceDragon.health).toEqual(4);
-  // })
+  test('should allow Lannister character to hit monster with additional strength', () => {
+    let charSkills2 = new Skills();
+    let jamieCharacter = new Character("Jamie", "Lannister", charSkills2);
+    jamieCharacter.addHouseBonus();
+    let battle2 = new Battlefield(jamieCharacter, iceDragon);
+    expect(battle2.characterHit()).toEqual("Ice Dragon Hit! Remaining Health: 1");
+  });
+
+  test('should allow monster to hit character according to strength stat', () => {
+    expect(battle.monsterHit()).toEqual("Larys Hit! Remaining Health: 12");
+  });
+
+  test('character with fire resistance should be hit 1 point less by fire element monsters', () => {
+    let charSkills2 = new Skills();
+    let raenyraCharacter = new Character("Raenyra", "Targaryen", charSkills2);
+    raenyraCharacter.addHouseBonus();
+    let fireDragon = new Monster("Fire Dragon", "fire", 8);
+    let battle2 = new Battlefield(raenyraCharacter, fireDragon);
+    expect(battle2.monsterHit()).toEqual("Raenyra Hit! Remaining Health: 13");
+  });
+
+  test('character with ice resistance should be hit 1 point less by ice element monsters', () => {
+    let charSkills2 = new Skills();
+    let nedCharacter = new Character("Ned", "Stark", charSkills2);
+    nedCharacter.addHouseBonus();
+    let battle2 = new Battlefield(nedCharacter, iceDragon);
+    expect(battle2.monsterHit()).toEqual("Ned Hit! Remaining Health: 13");
+  });
+
+  test('should return winning character message if monster health goes to 0', () => {
+    battle.monster.health = 0;
+    expect(battle.checkWinner()).toEqual("Larys Wins!");
+  });
+
+  test('should return winning character message if character health goes to 0', () => {
+    battle.character.health = 0;
+    expect(battle.checkWinner()).toEqual("Ice Dragon Wins!");
+  });
+
+  test('should return health points for each fighter if health is not 0', () => {
+    battle.character.health = 2;
+    battle.monster.health = 6;
+    expect(battle.checkWinner()).toEqual(`Remaining Health:
+Larys: 2
+Ice Dragon: 6`)});
+
+  test('should return dead message if both character and monster health are 0', () => {
+    battle.character.health = 0;
+    battle.monster.health = 0;
+    expect(battle.checkWinner()).toEqual("Alas! You both died a gruesome death!");
+  });
 });
 
 
--- src/js/battle.js
+++ src/js/battle.js
@@ -2,8 +2,6 @@ import {Character, Skills} from './characters.js'
 import {Monster} from './monsters.js'
 
 let charSkills = new Skills;
-let larysCharacter = new Character("Larys", "House", charSkills);
-let iceDragon = new Monster("ice", 8);
 
 
 export class Battlefield {
@@ -14,6 +12,41 @@ export class Battlefield {
 
   showInitialStats() {
     return `${this.character.name} vs ${this.monster.name}\nHealth:\n${this.character.health}HP vs ${this.monster.health}HP`
+  }
+
+  characterHit() {
+    if(this.character.special === 'Hit-advantage') {
+      this.monster.health -= (this.character.skills.strength + 1);
+      return `${this.monster.name} Hit! Remaining Health: ${this.monster.health}`;
+    } else {
+      this.monster.health -= this.character.skills.strength;
+      return `${this.monster.name} Hit! Remaining Health: ${this.monster.health}`;
+    }
+  }
+
+  monsterHit() {
+    if(this.character.special === 'Fire-resistant' && this.monster.special === "fire") {
+      this.character.health -= (this.monster.strength -1);
+      return `${this.character.name} Hit! Remaining Health: ${this.character.health}`;
+    } else if (this.character.special === 'Cold-resistant' && this.monster.special === "ice") {
+      this.character.health -= (this.monster.strength -1);
+      return `${this.character.name} Hit! Remaining Health: ${this.character.health}`;
+    } else {
+      this.character.health -= this.monster.strength;
+      return `${this.character.name} Hit! Remaining Health: ${this.character.health}`;
+    }
+  }
+
+  checkWinner() {
+    if (this.monster.health === 0 && this.character.health === 0) {
+      return `Alas! You both died a gruesome death!`
+    } else if (this.character.health === 0) {
+      return `${this.monster.name} Wins!`
+    } else if (this.monster.health === 0) {
+      return `${this.character.name} Wins!`
+    } else {
+      return `Remaining Health:\n${this.character.name}: ${this.character.health}\n${this.monster.name}: ${this.monster.health}`
+    }
   }
 //   displayStats(monster, character) {
 //     return `${character.name} : ${this.character.health}, ${monster.name} ${monster.health}`
